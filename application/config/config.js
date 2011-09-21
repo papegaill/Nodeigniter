6cbe607b33c170cd0af63a712e7a120afbcd16ed
root <root@NodeJs.(none)>
2011-09-21 23:01:10 +0000

    Updated config

--- application/config/config.js
+++ application/config/config.js
@@ -1,17 +1,22 @@
 exports.config = {
 	
 	url: {
-		base: '50.57.121.7:3000',
+	
+		base: '50.57.121.7',
 		baseNotSecure: 'http://' + this.base,
 		baseSecure: 'https://' + this.base,
+		
 		cdn: {
 			static: '',
 			
 		}
+		
 	},
 	
 	access: {
+	
 		whiteList: '127.0.0.1' // only allowed
+		
 	}
 	
 };
