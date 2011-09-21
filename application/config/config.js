17456797047ab700e76ad1634af7f30d1dbe3ec6
Scott Corgan <root@NodeJs.(none)>
2011-09-21 23:05:21 +0000

    Fixed mergin issues

--- application/config/config.js
+++ application/config/config.js
@@ -1,22 +1,18 @@
 exports.config = {
 	
 	url: {
-<<<<<<< HEAD
 	
 		base: '50.57.121.7',
 		baseNotSecure: 'http://' + this.base,
 		baseSecure: 'https://' + this.base,
-		
-=======
+
 		base: 'localhost:3000',
 		baseNotSecure: 'http://' + this.base,
 		baseSecure: 'https://' + this.base,
->>>>>>> 6c9ed3b8fc6f5884dc09180e91c6ac0d798c9eb9
 		cdn: {
 			static: '',
 			
 		}
-<<<<<<< HEAD
 		
 	},
 	
@@ -24,12 +20,6 @@ exports.config = {
 	
 		whiteList: '127.0.0.1' // only allowed
 		
-=======
-	},
-	
-	access: {
-		whiteList: '127.0.0.1' // only allowed
->>>>>>> 6c9ed3b8fc6f5884dc09180e91c6ac0d798c9eb9
 	}
 	
 };
--- application/config/db.js
+++ application/config/db.js
@@ -2,10 +2,6 @@ exports.db = {
 
 	driver:		'mongoskin',
 	hostname:	'localhost',
-<<<<<<< HEAD
 	database:	'nodeigniter'
-=======
-	database:	'node-mvc'
->>>>>>> 6c9ed3b8fc6f5884dc09180e91c6ac0d798c9eb9
 	
 }
